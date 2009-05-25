d2a54b207b6dff2e85cb583cb8961007ce7a8f68
behrenan <behrenan@470c4b18-1263-0410-9a7b-ece5b1bd733a>
2009-05-25 12:35:11 +0000

    Entfernungsberechnung funktioniert

--- trunk/app/controllers/entfernungen_controller.php
+++ trunk/app/controllers/entfernungen_controller.php
@@ -37,11 +37,64 @@ class EntfernungenController extends AppController
     }
     
     
-    public function Bogenmass($x)
+    /**
+     * 
+     * param $x = Angabe einer geografischen Position im Format 7° 25' N, 10° 59' O
+     * wird umgerechnet zu Bogenmaß als Array der Form
+     * array('lat','long'}
+     * wobei lat und long Floats sind. 
+     */
+    public function GeografischeGradzuBogenmass($x)
     {
-    	echo $x;
-    	return (($x * pi)/180);    	
+    	//Aufräumen des Ursprungstrings
+    	$x = str_replace('\'',' ',$x);
+    	$x = str_replace('°',' ',$x);
+    	$x = str_replace('"',' ',$x);
+    	$x = str_replace(',',' ',$x);
+    	
+    	//zerlegen in Token Umrechnung und Umrechnung in Grad
+    	$step = 0;
+    	$lat = 0;
+    	$lng = 0;
+    	$liste = explode(' ',$x);
+    	foreach ($liste as $token):
+   			$token = ltrim(rtrim($token));
+   			if (strlen($token)>0){
+    			switch($step){
+    				//Lat
+					case 0: if (is_numeric($token)){$lat = (int)$token; $step++; break;}
+					case 1: if (is_numeric($token)){$lat += (int)$token/60; $step++; break;}
+					case 2: if (is_numeric($token)){$lat += (int)$token/3600; $step++; break;}		
+					case 3: if (strtoupper($token) == 'S' ){$lat = $lat * -1;} $step = 4; break;
+					    
+					//Long				
+					case 4: if (is_numeric($token)){$lng = (int)$token; $step++; break;}
+					case 5: if (is_numeric($token)){$lng += (int)$token/60; $step++; break;}
+					case 6: if (is_numeric($token)){$lng += (int)$token/3600; $step++; break;}		
+					case 7: if (strtoupper($token) == 'W' ){$lng = $lng * -1;} $step = 9; break;
+
+					default: break;   				
+    			}
+   			}
+    	endforeach;
+    	return array($lat*pi()/180, $lng*pi()/180);
     }
+
+
+	/**
+	 * 
+	 * Test der Berechnung mit http://www.koordinaten.de/cgi-ko/koordinaten_entfernung.cgi
+	 */
+    public function CalcKugelDistanz($start, $ziel){
+    	$lat1 = $start[0];
+    	$lat2 = $ziel[0];
+    	$long1 = $start[1];
+    	$long2 = $ziel[1];
+    	
+ 		$km = acos(sin($lat1) * sin($lat2) + cos($lat1) * cos($lat2) * cos($long1- $long2) ) * 6378;
+ 		return round($km);
+    }
+
     
     /**
      * start: ID des StartFlugplatzes
@@ -51,13 +104,13 @@ class EntfernungenController extends AppController
     {
     	$this->Flugplatz->id=$start;
     	$startFlugplatz = $this->Flugplatz->field('geoPosition');
-    	$bmStart = $this->Bogenmass($startFlugplatz);
+    	$bmStart = $this->GeografischeGradzuBogenmass($startFlugplatz);
+    	
     	$this->Flugplatz->id=$ziel;
     	$zielFlugplatz = $this->Flugplatz->field('geoPosition');
+    	$bmZiel = $this->GeografischeGradzuBogenmass($zielFlugplatz);
     	
-    	
-    	srand((double)microtime()*1000000); 
-		return rand(0,100);
+    	return $this->CalcKugelDistanz($bmZiel,$bmStart);
     }
 }
 ?>
--- trunk/app/views/entfernung/index.ctp
+++ trunk/app/views/entfernung/index.ctp
@@ -1,5 +1,3 @@
-<h2>Entfernungsberechnungen</h2>
-
 <?php
     echo $form->create('Entfernung', array('action' => 'index', 'class'=>'yform columnar'));
     echo $form->input('start_id', array('div'=>'type-select', 'options' => $Flugplaetze));
@@ -7,36 +5,5 @@
     echo $form->input('Entfernung.Entfernung', array('type'=>'text','value'=>$Entfernung,'div'=>'type-text','disabled'=>'disabled'));
     
     echo $form->end(array('label'=>'Berechnen','div'=>'type-button'));
-
-	echo var_dump($this->data);
-
-/*
-    $modell = new Flugplatz(); //Modell fuer Flugzeugtyp erzeugen
-    $list = $modell->find('list');
-
-    echo $form->input('von', array('label'=>'Start','options' => $list));//auswahlbox anzeigen
-    echo $form->input('zu', array('label'=>'Ziel','options' => $list));//auswahlbox anzeigen
-
-    if (isset ($this->params) && isset ($this->params['data']) ) {
-        //Auslesen der uerbegebenen Parameter
-        $von = $this->params['data']['von'];
-        $zu = $this->params['data']['zu'];
-        echo $form->label('Entfernung', 'von ' . $von . ' nach ' . $zu);
-
-        //Suchen der eigentlichen Daten
-        $model = new Flugplatz();
-        $vonPositon = $model->findById($von);
-        $zuPositon = $model->findById($zu);
-
-        //Ausgabe der Position
-        echo $form->label('Von: '. $vonPositon['Flugplatz']['geoPosition']);
-        echo $form->label('Zu: '. $zuPositon['Flugplatz']['geoPosition']);
-    } else {
-        echo $form->label('Wählen Sie Start- und Zielflugplatz');
-    }
-
-    echo $form->submit('Berechnen');
-    echo $form->end();
-    */
 ?>
 
