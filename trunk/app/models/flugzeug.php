cebbb186746b7eb0ce145ce685d88ffb6eb90378
behrenan <behrenan@470c4b18-1263-0410-9a7b-ece5b1bd733a>
2009-06-22 21:57:17 +0000

    Fix: 15905
    wollte neues Flugzeug anlegen, nicht möglich, Fehlermeldung "SQL Error" etc. wird angezeigt.
    
    Testing Vorgehensweise:
    
    1.) Administration--> Flugzeuge
    2.) Flugzeuge anlegen
    3.) Selektion z.B. von Flugzeugtyp "GIV SP" und Kennzeichen eingebe
    
    Fehlerursache: Validierung fehlerhaft

--- trunk/app/models/flugzeug.php
+++ trunk/app/models/flugzeug.php
@@ -12,7 +12,9 @@ class Flugzeug extends AppModel {
 	//Das ist ein Array mit Valididierungsrichtilinien
 	//ist optional. Wenn nicht vorhanden, wird nicht
 	//validiert
-    public $validate = array('kennzeichen' => array('required'=>VALID_NOT_EMPTY, 'length'=>array('rule'=>array("maxLength"=>14))));
+    public $validate = array(
+	'kennzeichen' => array('notEmpty' => array('rule'=>'notEmpty', 'message'=>'Dieses Feld ist ein Pflichtfeld'),'between' => array('rule' => array('between', 0, 50),'message'=>'Der Name darf maximal 50 Zeichen lang sein'))
+	);
     public $displayField = 'kennzeichen';
 
 	//public $hasOne = 'Flugzeugtyp'; 
