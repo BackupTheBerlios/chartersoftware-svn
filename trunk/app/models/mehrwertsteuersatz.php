8c72d3d26d00754621d3faf33e5727418d7d95ce
behrenan <behrenan@470c4b18-1263-0410-9a7b-ece5b1bd733a>
2009-06-23 20:22:08 +0000

    Fix: 15920
    Fehler im Mehrwersteuer-Modell-Validator

--- trunk/app/models/mehrwertsteuersatz.php
+++ trunk/app/models/mehrwertsteuersatz.php
@@ -7,9 +7,31 @@ class Mehrwertsteuersatz extends AppModel {
     //validiert
     public $displayField = 'beschreibung';
     public $validate = array(
-        'beschreibung' => array('required'=>VALID_NOT_EMPTY, 'length'=>array('rule'=>array("maxLength"=>24))),
-        'satz' => array('required'=>VALID_NOT_EMPTY),
-        'scale' => array('required'=>VALID_NOT_EMPTY)
+		'beschreibung' => array('notEmpty' => array('rule'=>'notEmpty', 'message'=>'Dieses Feld ist ein Pflichtfeld'),'between' => array('rule' => array('between', 0, 50),'message'=>'Der Name darf maximal 50 Zeichen lang sein')),
+    	'scale'=>array(
+    		'numeric'=>array(
+    			'rule'=>'numeric',
+    			'required'=>true,
+    			'message'=>'Scale darf nur Zahlen enthalten und muss 1-5 Ziffern enthalten',
+    			'maxLength'=>5
+    		),
+    		'between' => array(
+				'rule' => array('between', 1, 5),
+    			'message'=>'Scale darf nur Zahlen enthalten und muss 1-5 Ziffern enthalten',
+    		)
+    	),
+    	'satz'=>array(
+    		'numeric'=>array(
+    			'rule'=>'numeric',
+    			'required'=>true,
+    			'message'=>'Satz darf nur Zahlen enthalten und muss 1-5 Ziffern enthalten',
+    			'maxLength'=>5
+    		),
+    		'between' => array(
+				'rule' => array('between', 1, 5),
+    			'message'=>'Satz darf nur Zahlen enthalten und muss 1-5 Ziffern enthalten',
+    		)
+    	)
     );
 }
 
