4aaa16a4be7a70ee223a0cf1bcf9fb30d4645892
behrenan <behrenan@470c4b18-1263-0410-9a7b-ece5b1bd733a>
2010-09-22 12:58:16 +0000

    neuroph integriert

--- trunk/abehrens/nn/src/de/oa/nn/trainer/network/Network.java
+++ trunk/abehrens/nn/src/de/oa/nn/trainer/network/Network.java
@@ -1,12 +1,20 @@
 package de.oa.nn.trainer.network;
 
+import java.util.ArrayList;
+
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.util.TransferFunctionType;
+
+import de.oa.system.Signal;
 
 public class Network {
   final MultiLayerPerceptron myMlPerceptron;
 
   public Network() {
-    myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 3, 1);
+    myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, 40, 400, 400, 3);
+  }
+
+  public void training(ArrayList<Signal> resultList) {
+
   }
 }
